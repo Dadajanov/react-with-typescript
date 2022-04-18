8bdd42024c2c1e6c7e66d1fe8d3d7fb953eb6e14
Sardorbek4198 <85184818+Sardorbek4198@users.noreply.github.com>
2022-04-18 22:33:00 +0500

    added functionality to add todo and delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.modal";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const handleDeleteTodo = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodos={handleAddTodo} />
+      <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+    </div>
+  );
+};
 
 export default App;
